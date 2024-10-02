9db7927ed2ecee5f23ee679b567e93714e7c087a
PriyaranjanAdhikari <padhikari2013@gmail.com>
2024-10-02 20:08:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const products = [
     {
-
         name: "Sukkhi Glamorous Gold Plated Choker Necklace Set Combo For Women",
 
         image: "https://m.media-amazon.com/images/I/912iX-Sve0L._SY695_.jpg",
@@ -8,7 +7,6 @@ const products = [
         link: "https://amzn.to/3zIt363",
     },
     {
-
         name: "amazon basics C12 12W Bluetooth Soundbar with 1200 mAh Battery | 2X Bass | Up to 12 hrs of Playback | Bluetooth 5.3, Aux & USB Connectivity (Black)",
 
         image: "https://m.media-amazon.com/images/I/71hlGt5UIlL._SX679_.jpg",
@@ -16,12 +14,38 @@ const products = [
         link: "https://amzn.to/3ZRmeK7",
     },
     {
+        name: "boAt Storm Call 3 w/Turn-by-Turn Navigation, 1.83” (4.6 cm) HD Display, Bluetooth Calling, Crest+ OS, QR Tray, Watch Face Studio, Coins, Emergency SOS Smart Watch for Men & Women(Active Black)",
 
-        name: "Dell [Smartchoice] Core i3-1215U, 12th Gen (8GB RAM/512GB SSD/FHD/Window 11/MS Office' 21/15"(38 cm)/15 Month McAfee/Black/1.48kg Laptop",
+        image: "https://m.media-amazon.com/images/I/61A9rFdBjtL._SX679_.jpg",
+        price: "₹999",
+        link: "https://amzn.to/4gPCL7C",
+    },
+    {
+        name: "GoSriKi Women's Rayon Blend Anarkali Printed Kurta with Palazzo & Dupatta",
 
-        image: "https://m.media-amazon.com/images/I/61+4uIceaAL._SX679_.jpg",
-        price: "₹33990",
-        link: "https://amzn.to/3Ng6SHr",
+        image: "https://m.media-amazon.com/images/I/71mX4WATh-L._SX679_.jpg",
+        price: "₹689",
+        link: "https://amzn.to/3Bpt0wG",
+    },
+    {
+        name: "Noise Twist Round dial Smart Watch with Bluetooth Calling, 1.38” TFT Display, up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate Monitor, Sleep Tracking (Jet Black)",
+        image: "https://m.media-amazon.com/images/I/61TapeOXotL._SX679_.jpg",
+        price: "₹1199",
+        link: "https://amzn.to/3XLg8sd",
+    },
+    {
+        name: "ZENEME Bangle Gold Plated American Studded Royal Style Broad Golden Antique Kada Bangle Jewellery Combo Of 2 For Girls & Women",
+
+        image: "https://m.media-amazon.com/images/I/71bOhZUE64L._SY695_.jpg",
+        price: "₹222",
+        link: "https://amzn.to/4dqUO0J",
+    },
+    {
+        name: "Bata Men's Fortuner Sport Sandal",
+
+        image: "https://m.media-amazon.com/images/I/612AwnA1iDL._SX695_.jpg",
+        price: "₹281",
+        link: "https://amzn.to/47RZ4Fw",
     },
     {
 
@@ -54,59 +78,12 @@ const products = [
     },
     {
 
-        name: "boAt Storm Call 3 w/Turn-by-Turn Navigation, 1.83” (4.6 cm) HD Display, Bluetooth Calling, Crest+ OS, QR Tray, Watch Face Studio, Coins, Emergency SOS Smart Watch for Men & Women(Active Black)",
-
-        image: "https://m.media-amazon.com/images/I/61A9rFdBjtL._SX679_.jpg",
-        price: "₹999",
-        link: "https://amzn.to/4gPCL7C",
-    },
-    {
-
-        name: "GoSriKi Women's Rayon Blend Anarkali Printed Kurta with Palazzo & Dupatta",
-
-        image: "https://m.media-amazon.com/images/I/71mX4WATh-L._SX679_.jpg",
-        price: "₹689",
-        link: "https://amzn.to/3Bpt0wG",
-    },
-    {
-        name: "Noise Twist Round dial Smart Watch with Bluetooth Calling, 1.38” TFT Display, up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate Monitor, Sleep Tracking (Jet Black)",
-        image: "https://m.media-amazon.com/images/I/61TapeOXotL._SX679_.jpg",
-        price: "₹1199",
-        link: "https://amzn.to/3XLg8sd",
-    },
-    {
-
-        name: "Acer Aspire Lite 13th Gen Intel Core i3-1305U Thin and Light Premium Laptop (Windows 11 Home/8 GB RAM/512GB SSD/36 WHR) AL15-53 with 39.62cm (15.6") Full HD Display, Metal Body, Steel Gray, 1.59 KG",
-
-        image: "https://m.media-amazon.com/images/I/61fDHkQ6MqL._SX679_.jpg",
-        price: "₹29990",
-        link: "https://amzn.to/3ZOGR9R",
-    },
-    {
-
-        name: "ZENEME Bangle Gold Plated American Studded Royal Style Broad Golden Antique Kada Bangle Jewellery Combo Of 2 For Girls & Women",
-
-        image: "https://m.media-amazon.com/images/I/71bOhZUE64L._SY695_.jpg",
-        price: "₹222",
-        link: "https://amzn.to/4dqUO0J",
-    },
-    {
-
-        name: "Bata Men's Fortuner Sport Sandal",
-
-        image: "https://m.media-amazon.com/images/I/612AwnA1iDL._SX695_.jpg",
-        price: "₹281",
-        link: "https://amzn.to/47RZ4Fw",
-    },
-    {
-
         name: "boAt Rockerz 450 Bluetooth On Ear Headphones with Mic, Upto 15 Hours Playback, 40MM Drivers, Padded Ear Cushions, Integrated Controls and Dual Modes(Luscious Black)",
 
         image: "https://m.media-amazon.com/images/I/51FNnHjzhQL._SX679_.jpg",
         price: "₹1298",
         link: "https://amzn.to/3zxiwL9",
     },
-
 ];
 
 let rewardPoints = 0;
